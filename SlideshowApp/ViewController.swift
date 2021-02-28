5393eef44b71e8ba7de786920a44aed960a1efec
kazuki 9425 <lexusrx2017@yahoo.co.jp>
2021-02-28 17:34:38 +0900

    課題

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,124 @@
 import UIKit
 
 class ViewController: UIViewController {
+  //outlletの接続
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var susumu: UIButton!
+    @IBOutlet weak var modoru: UIButton!
+   
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        }
+    
+    var timer: Timer!
+    var timer_sec: Float = 0
+    var imageNo = 0
+    
+    let imageName = ["S__img1.jpg", "S__img2.jpg", "S__img3.jpg"]
+    
+  //進むボタンを押した時の動作
+    
+    @IBAction func next(_ sender: Any) {
+        if imageNo == 0 {
+            imageNo = 1
+        }else if imageNo == 1 {
+            imageNo = 2
+        }else if imageNo == 2 {
+            imageNo = 0
+        }
+        let name = imageName[imageNo]
+        
+        imageView.image = UIImage(named:name)
+        
+    }
+        
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //戻るボタンを押した時の動作
+        @IBAction func back(_ sender: Any) {
+        
+            if imageNo == 1 {
+                imageNo = 0
+            }else if imageNo == 2 {
+                imageNo = 1
+            }else if imageNo == 0 {
+                imageNo = 2
+            }
+       
+            let name = imageName[imageNo]
+            
+            imageView.image = UIImage(named:name)
+    
+    }
+    
+   
+                        
+
+    
+    
+            
+override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    imageView.image = UIImage(named: "S__img1.jpg")
+}
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // segueから遷移先のSecoundを取得する
+        let secound:secound = segue.destination as! secound
+        //画像のデータを渡す
+        secound.name2 = imageView.image!
+        
+    }
+    
+    //スライドが2秒毎に切り替わる動作
+    @objc func updateTimer(_ timer: Timer) {
+        self.timer_sec += 2
+       
+        if imageNo == 0 {
+            imageNo = 1
+        }else if imageNo == 1 {
+            imageNo = 2
+        }else if imageNo == 2 {
+            imageNo = 0
+        }
+        let name = imageName[imageNo]
+        
+        imageView.image = UIImage(named:name)
+        
     }
 
+    
+    
+        //再生の動作//停止の動作
+    @IBAction func suraidshow(_ sender: Any) {
+    
+       //タイマーの設定
+        if self.timer == nil {
+        self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self,selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+       //UIButtonを無効化
+        susumu.isEnabled = false
+      //UIButtonを無効化
+        modoru.isEnabled = false
+       
+        } else {
+             if self.timer != nil {
+            self.timer.invalidate()   // タイマーを停止する
+            self.timer = nil
+                
+                //UIOutletを有効化
+                susumu.isEnabled = true
+               //UIOutlletを有効化
+                modoru.isEnabled = true
+           
+       }
+    
+       }
 
 }
+
+}
+       
+           
+       
+
+    
 
---
+++ SlideshowApp/secound.swift
@@ -0,0 +1,37 @@
+//
+//  secound.swift
+//  SlideshowApp
+//
+//  Created by 傳田和樹 on 2021/02/21.
+//
+
+import UIKit
+
+class secound: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var name2: UIImage = UIImage()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+  
+        
+         imageView.image = name2
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
